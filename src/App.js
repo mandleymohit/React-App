ab82c2d945688d80ea4ed8186dd0a2f22339afe0
mandleymohit <110674276+mandleymohit@users.noreply.github.com>
2022-08-08 21:46:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Development is happening here.
+          Development is happening.
         </p>
         <a
           className="App-link"
